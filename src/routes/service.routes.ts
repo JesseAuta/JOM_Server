8622d585ef2e05a735e3ae1cd40972043779b661
Olena <olena.demchenko@dci-student.org>
2026-03-10 14:44:46 +0100

    controller and routes for adminpanel services

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -1,5 +1,21 @@
+// import { Router } from 'express';
+// import { getServices } from '../controllers/service.controller';
+// const router = Router();
+// router.get('/', getServices);
+// export default router;
 import { Router } from 'express';
-import { getServices } from '../controllers/service.controller';
+import {
+  getServices,
+  createService,
+  updateService,
+  deleteService,
+} from '../controllers/admin_service.controller';
+
 const router = Router();
-router.get('/', getServices);
+
+router.get('/', getServices); // GET all services
+router.post('/', createService); // CREATE service
+router.put('/:id', updateService); // UPDATE service by id
+router.delete('/:id', deleteService); // DELETE service by id
+
 export default router;
